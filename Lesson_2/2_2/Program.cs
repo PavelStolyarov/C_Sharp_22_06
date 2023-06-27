30f8170eadecfa914bc112117e826755c3b47c52
Павел <pashastoliarov442@gmail.com>
2023-06-27 12:45:21 +0300

    feat: complete 2 task

---
+++ Lesson_2/2_2/Program.cs
@@ -0,0 +1,18 @@
+﻿int Max(int count)
+{
+    int ones = count % 10;
+    int tens = count / 10;
+    Console.WriteLine(tens);
+    if(ones > tens)
+    {
+        return ones;
+    }
+    else
+    {
+        return tens;
+    }
+}
+int num = new Random().Next(10, 99);
+Console.WriteLine($"Generated number: {num} " );
+int result = Max(num);
+Console.WriteLine(result);
