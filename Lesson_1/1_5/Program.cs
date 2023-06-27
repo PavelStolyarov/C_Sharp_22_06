a0318b095a71d9a73331c1bb8f9b18ce41a794a5
Павел <pashastoliarov442@gmail.com>
2023-06-27 12:18:06 +0300

    feat: classwork

---
+++ Lesson_1/1_5/Program.cs
@@ -0,0 +1,23 @@
+﻿// 3. Напишите программу, которая на вход принимает одно число
+//    (N), а на выходе показывает все целые числа в промежутке от -N до N.
+Console.Write("Enter number: ");
+int num1 = int.Parse(Console.ReadLine()!);
+int num2 = num1 * -1;
+if(num1 >= 0)
+{
+    while(num2 <= num1)
+    {
+        Console.Write(num2);
+        Console.Write(" ");
+        num2++;
+    }
+}
+else
+{
+    while(num2 >= num1)
+    {
+        Console.Write(num2);
+        Console.Write(" ");
+        num2--;
+    }
+}
---
+++ Lesson_2/2_0/Program.cs
@@ -0,0 +1,19 @@
+﻿// 0. Напишите программу, которая принимает на вход трёхзначное число
+// и на выходе показывает последнюю цифру этого числа.
+
+
+/* int num = int.Parse(Console.ReadLine()!);
+int num1 = num % 10;
+Console.WriteLine(num1); */
+
+
+int TakeNum(int num)
+{
+
+    Console.WriteLine(num);
+    return num % 10;
+}
+
+int th_num = new Random().Next(100, 1000);
+int result = TakeNum(th_num);
+Console.WriteLine(result);
